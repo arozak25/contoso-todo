c44c476c3b18e30ebdb28dcf7ffb5d582172186a
evenuss <ucoksilalahi345@gmail.com>
2019-12-17 13:36:46 +0700

    Revisi 4

--- main.py
+++ main.py
@@ -37,11 +37,20 @@ def CreateUser():
         updatedAt = datetime.now()
 
     if existing_user is None:
-<<<<<<< HEAD
-        mongo.db.user.insert({'userId': UserId ,'fullName': fullName,'email': email, 'password': pw_hash,'address':address,'phoneNumber':phoneNumber,'role':role,'verified':verified,'profilePictureUrl':profilePictureUrl,'createdAt':createdAt,'updatedAt':updatedAt})
-=======
-        mongo.db.user.insert({'UserId': UserId ,'fullName': fullName,'email': email, 'password': pw_hash,'address':address,'phoneNumber':phoneNumber,'role':role,'verified':verified,'profilePictureUrl':'-','createdAt':createdAt,'updatedAt':updatedAt})
->>>>>>> 5185d4c6e6fe59621880f95bf96f0d9449c2c606
+        mongo.db.user.insert({
+            'userId': UserId ,
+            'fullName': fullName,
+            'email': email,
+            'password': pw_hash,
+            'address':address,
+            'phoneNumber':phoneNumber,
+            'role':role,
+            'verified':verified,
+            'profilePictureUrl':profilePictureUrl,
+            'createdAt':createdAt,
+            'updatedAt':updatedAt
+        })
+
         return jsonify({'message':'Registrasi berhasil !'})
 
     return jsonify({'message':'Email already exists'})
@@ -102,10 +111,16 @@ def editData():
         email = request.json['email']
 
     updatequery = {'email': email}
-    newvalues = {'$set': {'fullName': fullName, 'address': address, 'phoneNumber': phoneNumber,
-                                  'profilePictureUrl': profilePictureUrl}}
+    newvalues = {'$set':
+                     {'fullName': fullName,
+                      'address': address,
+                      'phoneNumber': phoneNumber,
+                      'profilePictureUrl': profilePictureUrl
+                      }
+                 }
     mongo.db.user.update_one(updatequery, newvalues)
     return jsonify({'message': 'Edit berhasil'})
+
 
 @app.route('/forgetpassword', methods=['POST'])
 def SendEmailForgetPassword():
@@ -140,10 +155,15 @@ def updateForgetPassword():
 
     return jsonify({'message':'success'})
 
+
+
+
+
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return jti in blacklist
+
 
 
 
@@ -211,10 +231,13 @@ def newTask(userid):
                     "updatedAt":updatedAt
                 })
         if new_task and request.method == 'POST':
-            return jsonify(new_task)
+            return jsonify('Success!')
     except Exception as e:
         return e
 
+
+
+
 @app.route('/showall', methods=['POST','GET'])
 @jwt_required
 def showalltodolist():
@@ -237,6 +260,8 @@ def showalltodolist():
     return resp
 
 
+
+
 @app.route('/delete', methods=['PUT'])
 def delete():
     tododelete = ["dandung","arjuna","arya"]
@@ -245,6 +270,8 @@ def delete():
         newvalues = {'$set': {'deleted': True}}
         mongo.db.todo.update_one(updatequery, newvalues)
     return jsonify({'message':'success','status':200})
+
+
 
 
 @app.route('/todolist/page', methods = ['GET'])
@@ -265,64 +292,6 @@ def pagination():
 
     for i in pagination:
         output.append({'name': i['name'],'description': i['description'],'date': i['date'],'favorite': i['favorite'],'deleted': i['deleted'],'createdAt': i['createdAt'],'updatedAt': i['updatedAt']})
-
-    next_url = '/todolist/page?limit=' + str(limit) + '&offset=' + str(offset + limit)
-    prev_url = '/todolist/page?limit=' + str(limit) + '&offset=' + str(offset - limit)
-
-<<<<<<< HEAD
-@app.route('/showall', methods=['POST','GET'])
-@jwt_required
-def showalltodolist():
-    user_id = "100"
-    todolist = mongo.db.todo.find({'userId': user_id,'deleted':False})
-    result = []
-    for alltodo in todolist:
-        result.append({
-            'toDoId':str(alltodo['toDoId']),
-            'name':alltodo['name'],
-            'description':alltodo['description'],
-            'date':alltodo['date'],
-            'favorite': alltodo['favorite'],
-            'completed': alltodo['completed'],
-            'deleted': alltodo['deleted'],
-            'createdAt': alltodo['createdAt'],
-            'updatedAt': alltodo['updatedAt'],
-        })
-    resp = jsonify({'result':result})
-    return resp
-
-
-@app.route('/delete', methods=['PUT'])
-def delete():
-    tododelete = ["dandung","arjuna","arya"]
-    for bulkdelete in tododelete:
-        updatequery = {'name': tododelete}
-        newvalues = {'$set': {'deleted': True}}
-        mongo.db.todo.update_one(updatequery, newvalues)
-    return jsonify({'message':'success','status':200})
-
-
-@app.route('/todolist/page', methods = ['GET'])
-@jwt_required
-def pagination():
-
-    user_id = "100"
-    number = mongo.db.todo
-
-    offset = int(request.args['offset'])
-    limit = int(request.args['limit'])
-
-    starting_id = number.find({'userId': user_id,'deleted':False}).sort('_id', pymongo.ASCENDING)
-    last_id= starting_id[offset]['_id']
-
-    pagination = number.find({'_id' : {'$gte': last_id}}).sort('_id', pymongo.ASCENDING).limit(limit)
-    output = []
-
-    for i in pagination:
-        output.append({'name': i['name'],'description': i['description'],'date': i['date'],'favorite': i['favorite'],'deleted': i['deleted'],'createdAt': i['createdAt'],'updatedAt': i['updatedAt']})
-=======
-    return jsonify({'result': output,'prev_url': prev_url ,'next_url': next_url})
->>>>>>> 5185d4c6e6fe59621880f95bf96f0d9449c2c606
 
     next_url = '/todolist/page?limit=' + str(limit) + '&offset=' + str(offset + limit)
     prev_url = '/todolist/page?limit=' + str(limit) + '&offset=' + str(offset - limit)
@@ -346,12 +315,19 @@ def updateTask(todoid,userid):
     except Exception as e:
         return e
 
+
+
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_fille():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
       return 'file uploaded successfully'
+
+
+
+
 
 @app.route('/upload',methods = ['GET','POST'])
 def upload_file():
